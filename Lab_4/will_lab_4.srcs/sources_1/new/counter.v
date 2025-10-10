e665265756395f5a3680bc503b9b48ef50bff0b2
Will-est <rwillpainter@gmail.com>
2025-10-10 15:27:14 -0500

    made it so there is only one counter

--- Lab_4/will_lab_4.srcs/sources_1/new/counter.v
+++ Lab_4/will_lab_4.srcs/sources_1/new/counter.v
@@ -1,19 +1,22 @@
 `timescale 1ns / 1ps
 
 module counter (
+    // inputs
     input wire fsm_out,             // trigger from FSM module
     input wire clk_1hz,             // 1Hz clock signal
-    input wire button,              // button input
-    input wire switch,              // switch selector
+    input wire button_en,              // button input
+    input wire switch_en,              // switch selector
     input wire [13:0] add_val,      // value to add when button pressed
     input wire [1:0] switch_values, // array of 2 reset values
+    
+    // outputs
     output reg [13:0] count_reg     // stored counter value
 );
 
     initial count_reg = 14'd0;
 
     always @(posedge fsm_out) begin
-        case ({switch, button, clk_1hz})
+        case ({switch_en, button_en, clk_1hz})
             3'b000: count_reg <= count_reg;                                                                // hold
             3'b001: count_reg <= (count_reg > 14'd0)    ? count_reg - 14'd1 : 14'd0;                       // clk only
             3'b010: count_reg <= (count_reg < 14'd9999) ? count_reg + add_val : 14'd9999;                  // button only
