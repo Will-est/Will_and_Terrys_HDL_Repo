b0e0b5e712382909c34fea6de77d40e336e6fec7
terrytan23 <terrytan0606@gmail.com>
2025-10-11 02:19:02 -0500

    seven_seg_mux done

--- Lab_4/project_1.srcs/sources_1/new/sevenseg_mux.v
+++ Lab_4/project_1.srcs/sources_1/new/sevenseg_mux.v
@@ -52,4 +52,24 @@ module sevenseg_mux(
             endcase
         end
     end
+    
+    function [6:0] seg7_decode; // {a,b,c,d,e,f,g}, adjust for your polarity
+        input [3:0] d;
+        case (d)
+            4'd0: seg7_decode = 7'b0000001;
+            4'd1: seg7_decode = 7'b1001111;
+            4'd2: seg7_decode = 7'b0010010;
+            4'd3: seg7_decode = 7'b0000110;
+            4'd4: seg7_decode = 7'b1001100;
+            4'd5: seg7_decode = 7'b0100100;
+            4'd6: seg7_decode = 7'b0100000;
+            4'd7: seg7_decode = 7'b0001111;
+            4'd8: seg7_decode = 7'b0000000;
+            4'd9: seg7_decode = 7'b0000100;
+            default: seg7_decode = 7'b1111110; // dash/blank as needed
+        endcase
+    endfunction
 endmodule
+
+
+
