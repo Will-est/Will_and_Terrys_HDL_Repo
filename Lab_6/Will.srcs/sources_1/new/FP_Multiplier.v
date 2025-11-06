563711465d23a9e108990cec4530c7bff3871384
Will-est <rwillpainter@gmail.com>
2025-11-06 02:25:41 -0600

    Made skeleton of lab 6

---
+++ Lab_6/Will.srcs/sources_1/new/FP_Multiplier.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/06/2025 02:19:04 AM
+// Design Name: 
+// Module Name: FP_Multiplier
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module FP_Multiplier(
+
+    );
+endmodule
