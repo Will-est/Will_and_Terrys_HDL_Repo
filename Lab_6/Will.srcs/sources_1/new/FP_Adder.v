852cf89531710812113519a46bcc01c4e7d0c23e
terrytan23 <terrytan0606@gmail.com>
2025-11-12 20:23:51 -0600

    middle of fixing, swiching computers

--- Lab_6/Will.srcs/sources_1/new/FP_Adder.v
+++ Lab_6/Will.srcs/sources_1/new/FP_Adder.v
@@ -25,71 +25,52 @@ module FP_Adder (
     input  [7:0] b,
     output [7:0] out
 );
-
-    // Decompose input A
-    wire signA = a[7];
-    wire [2:0] expA = a[6:4];
-    wire [3:0] fracA = a[3:0];
-
-    // Decompose input B
-    wire signB = b[7];
-    wire [2:0] expB = b[6:4];
-    wire [3:0] fracB = b[3:0];
-
-    // Add hidden bit (1 for normalized values)
-    wire [5:0] mantA = (|expA) ? {1'b1, fracA} : {1'b0, fracA};
-    wire [5:0] mantB = (|expB) ? {1'b1, fracB} : {1'b0, fracB};
-
-    // Align exponents
-    wire [2:0] expDiff_AB = (expA >= expB) ? (expA - expB) : (expB - expA);
-    wire swap = (expB > expA);
-
-    wire [5:0] mant_large = swap ? mantB : mantA;
-    wire [5:0] mant_small = swap ? mantA : mantB;
-    wire sign_large = swap ? signB : signA;
-    wire sign_small = swap ? signA : signB;
-    wire [2:0] exp_large = swap ? expB : expA;
-
-    // Shift smaller mantissa for exponent alignment
-    wire [5:0] mant_small_shifted = mant_small >> expDiff_AB;
-
-    // Add or subtract mantissas depending on sign
-    wire [6:0] mant_sum = (sign_large == sign_small) ?
-                          ({1'b0, mant_large} + {1'b0, mant_small_shifted}) :
-                          ({1'b0, mant_large} - {1'b0, mant_small_shifted});
-
-    // Normalization
-    reg [5:0] mant_norm;
-    reg [2:0] exp_norm;
-    reg sign_out;
-
-    always @(*) begin
-        sign_out = sign_large;
-        exp_norm = exp_large;
-
-        if (mant_sum[6]) begin
-            // Mantissa overflow → shift right, increment exponent
-            mant_norm = mant_sum[6:1];
-            exp_norm = exp_large + 1;
-        end else if (mant_sum[5:1] == 0) begin
-            // Zero result
-            mant_norm = 0;
-            exp_norm = 0;
-            sign_out = 0;
-        end else begin
-            // Normalized mantissa
-            mant_norm = mant_sum[5:0];
-            // Normalize if MSB not 1
-            if (!mant_norm[5] && exp_norm > 0) begin
-                mant_norm = mant_norm << 1;
-                exp_norm = exp_norm - 1;
-            end
-        end
-    end
-
-    // Pack result: [sign | exponent | fraction]
-    assign out = {sign_out, exp_norm, mant_norm[4:1]};
-
+    // unpack wires
+    wire a_s = a[7];
+    wire [2:0] a_e = a[6:4];
+    wire [3:0] a_f = a[3:0];
+    
+    wire b_s = b[7];
+    wire [2:0] b_e = b[6:4];
+    wire [3:0] b_f = b[3:0];
+    
+    localparam BIAS = 3;
+    
+    // zeroes
+    wire a_zero = (a_e == 3'b000);
+    wire b_zero = (b_e == 3'b000);
+    
+    wire a_sig = a_zero ? 5'b0 : {1'b1, a_f};
+    wire b_sig = b_zero ? 5'b0 : {1'b1, b_f};
+    
+    // which operand is bigger 
+    wire a_gt = (a_e > b_e) || ((a_e == b_e) && (a_sig >=  b_sig));
+    
+    wire x_s = a_gt ? a_s : b_s;
+    wire [2:0] x_e = a_gt ? a_e : b_e;
+    wire [4:0] x_sig = a_gt ? a_sig : b_sig;
+    
+    wire y_s = a_gt ? b_s : a_s;
+    wire [2:0] y_e = a_gt ? b_e : a_e;
+    wire [4:0] y_sig = a_gt ? b_sig : a_sig;
+    
+    // alignment
+    wire [3:0] exp_diff_uncapped = (x_e >= y_e) ? (x_e - y_e) : 4'b0;
+    wire [2:0] exp_diff = (exp_diff_uncapped > 3'd5) ? 3'd5: exp_diff_uncapped[2:0]; // only 5 shifts matter
+    
+    // right shift w sticky bit
+    reg [4:0] y_sig_aligned;
+    reg y_sticky;
+    integer i;
+    
+    always @* begin:
+        y_sig_aligned = y_sig;
+        y_sticky = 1'b0;
+        if (exp_diff
+        
+    
+    
+    
 endmodule
 
 
