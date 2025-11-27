302c5af5a2513df8fd752dcb4260d1a92811e676
Will-est <rwillpainter@gmail.com>
2025-11-27 02:40:00 -0600

    Brought over starter design files

--- Lab_7/Will.srcs/sources_1/new/top.v
+++ Lab_7/Will.srcs/sources_1/new/top.v
@@ -20,7 +20,17 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module top(
+module top(CLK, RST);
+// This is your top module for synthesis.
+  // You define what signals the top module needs.
+  input CLK;
+  input RST;
 
-    );
+  wire CS, WE;
+  wire [6:0] ADDR;
+  wire [31:0] Mem_Bus;
+
+  MIPS CPU(CLK, RST, CS, WE, ADDR, Mem_Bus);
+  Memory MEM(CS, WE, CLK, ADDR, Mem_Bus);
+
 endmodule
